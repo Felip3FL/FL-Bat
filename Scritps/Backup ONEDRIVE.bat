4f21957939b8902c36b840481e40eeef160df9dc
Felip3FL <felip3.fl@gmail.com>
2019-11-08 22:24:41 -0300

    Create Backup ONEDRIVE.bat

---
+++ Scritps/Backup ONEDRIVE.bat
@@ -0,0 +1,8 @@
+REM NINTENDO WII U - CEMU
+Robocopy "C:\Nintendo Wii U\CEMU\mlc01\usr\save"        "C:\Users\felip\OneDrive\Documents\My Games\CEMU\mlc01\usr\save" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\mlc01\sys"             "C:\Users\felip\OneDrive\Documents\My Games\CEMU\mlc01\sys" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\controllerProfiles"    "C:\Users\felip\OneDrive\Documents\My Games\CEMU\controllerProfiles" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\gameProfiles"          "C:\Users\felip\OneDrive\Documents\My Games\CEMU\gameProfiles" /mir /z
+
+REM METAL GEAR REVENGEANCE
+Robocopy "C:\Users\felip\Documents\MGR" "C:\Users\felip\OneDrive\Documents\My Games\MGR" /mir /z
